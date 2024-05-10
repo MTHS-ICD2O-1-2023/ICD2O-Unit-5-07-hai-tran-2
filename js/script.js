80dc1bb8bdbdf480e997523b05466a1cc104a1c4
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-05-10 15:26:06 +0000

    add webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function calculateSumOfNNumbers() {
+  // input
+  let numberAddingUpTo = parseFloat(document.getElementById("number-adding-to").value)
+  let sumOfNUNumber = 0
+
+  // process
+  for (let counter = 0; counter <= numberAddingUpTo; counter++) {
+    // output
+    sumOfNUNumber = sumOfNUNumber + counter
+    document.getElementById("result").innerHTML = "The sum of two number is " + sumOfNUNumber
+  }
+}
